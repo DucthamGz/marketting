a18afd26e3fc61b803d8d7d5d81d81574b143d44
Duc_Tham <Ductham784@gmail.com>
2023-12-13 11:57:19 +0700

    header

---
+++ publics/js/main.js
@@ -0,0 +1,7 @@
+// HEADER START
+const activeMenu = (element) => {
+    $('body').toggleClass('active');
+    $(".btn_menu_mobile").toggleClass("active");
+    $(".menu_move").toggleClass("active");
+}
+
